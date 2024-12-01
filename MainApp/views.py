b0e67d0fa2a428565784030e58361a83966f4cb3
Sergej pakhomov <psv.ekb@gmail.com>
2024-12-01 20:17:19 +0300

    add first url/view

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    text = '''<h1>"Изучаем django"</h1>
+<strong>Автор</strong>: <i>Пахомов С.В.</i>
+'''
+    return HttpResponse(text)
