031b7cf10dc1527623e9b8f401d041cbe87b23b9
Sergej pakhomov <psv.ekb@gmail.com>
2024-12-03 17:43:48 +0300

    orm-part2

--- MainApp/models.py
+++ MainApp/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
+    desc = models.CharField(max_length=100, default="Описание товара")
     count = models.PositiveIntegerField()
