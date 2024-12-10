b075dee2b3c058faa0dc05f86fa18d2d17d116bb
Sergej pakhomov <psv.ekb@gmail.com>
2024-12-10 19:46:38 +0300

    add path style templates

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -18,11 +18,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from MainApp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path("", views.home),
-    path("about", views.about),
-    path("item/<int:id>", views.item),
-    path("items", views.get_items),
-]
+    path("", views.home, name='home'),
+    path("about", views.about, name='about'),
+    path("item/<int:id>", views.item, name='item-detail'),
+    path("items", views.get_items, name='items-list')
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
